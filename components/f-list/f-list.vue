88339cf0a6c42193a21bdf1a3ce3f6e857f9a247
jinchenxing <2541226493@qq.com>
2023-07-04 00:54:53 +0800

    文件下载上传状态列表

--- components/f-list/f-list.vue
+++ components/f-list/f-list.vue
@@ -1,12 +1,18 @@
 <script setup>
-	import {computed, reactive} from "vue"
-	
+	import {
+		computed,
+		reactive
+	} from "vue"
+
 	const props = defineProps({
 		item: Object,
 		index: [Number, String],
-		checked: Boolean
+		showRight: {
+			type: Boolean,
+			default: true
+		}
 	})
-	
+
 	const icons = reactive({
 		dir: {
 			icon: 'icon-file-b-2',
@@ -29,41 +35,49 @@
 			color: 'text-muted'
 		}
 	})
-	
+
 	const iconClass = computed(() => {
 		// 根据传入的item元素的type属性，获取对应的icon对象
 		let item = icons[props.item.type]
 		// 根据取得的item对象，拼接class名
 		return `${item.icon} ${item.color}`
 	})
-	
+
 	// 定义向父组件抛出的事件
 	const emits = defineEmits(['my-select'])
-	
+
 	const onSelect = () => {
 		emits('my-select')
 	}
-</script>	
-	
+</script>
+
 <template>
-	<view class="p-3 flex align-center border bottom-border" @click="emits('click')">
-		
-		<!-- 左侧：用计算属性得出动态样式，显示不同的文件图标 -->
-		<text class="iconfont" :class="iconClass" style="font-size: 60rpx;"></text>
-		
-		<!-- 中间：渲染父组件传入的对象中的名称和事件 -->
-		<view class="flex flex-column ml-3" style="line-height: 1.2;">
-			<text class="font-md">{{ props.item.name }}</text>
-			<text class="font-sm text-muted mt-2">{{ props.item.create_time }}</text>
+	<view>
+		<view class="p-3 flex align-center border bottom-border" @click="emits('click')">
+			<!-- 左侧：用计算属性得出动态样式，显示不同的文件图标 -->
+			<text class="iconfont" :class="iconClass" style="font-size: 60rpx;"></text>
+
+			<!-- 中间：渲染父组件传入的对象中的名称和事件 -->
+			<view class="flex flex-column ml-3" style="line-height: 1.2;">
+				<text class="font-md">{{ props.item.name }}</text>
+				<text class="font-sm text-muted mt-2">{{ props.item.create_time }}</text>
+			</view>
+
+			<!-- 右侧：根据传入对象的checked属性进行渲染,如果 showRight 为true，显示默认插槽-->
+			<view class="ml-auto" v-if="props.showRight">
+				<slot>
+					<view class="ml-auto flex align-center justify-center" @click.stop="onSelect">
+						<!-- 未选择，灰色圈圈 -->
+						<text v-if="!props.item.checked" style="width: 25rpx; height: 25rpx;"
+							class="rounded-circle border"></text>
+						<!-- 选中，字体图标 -->
+						<text v-else class="iconfont icon-xuanze-yixuan text-primary" style="font-size: 40px;"></text>
+					</view>
+				</slot>
+			</view>
 		</view>
-		
-		<!-- 右侧：根据传入对象的checked属性进行渲染 -->
-		<view class="ml-auto flex align-center justify-center" @click.stop="onSelect">
-			<!-- 未选择，灰色圈圈 -->
-			<text v-if="!props.item.checked" style="width: 30rpx; height: 30rpx; border: 1px solid #999;" class="rounded-circle"></text>
-			<!-- 选中，字体图标 -->
-			<text v-else class="iconfont icon-xuanze-yixuan text-primary" style="font-size: 20px;"></text>
-		</view>
+
+		<!-- bottom具名插槽 -->
+		<slot name="bottom"></slot>
 	</view>
 </template>
-
--- pages/list/list.vue
+++ pages/list/list.vue
@@ -1,22 +1,131 @@
-<template>
-	<view>
-		
-	</view>
-</template>
-
-<script>
-	export default {
-		data() {
-			return {
-				
-			}
+<script setup>
+	import {
+		computed,
+		ref
+	} from "vue";
+	const tabIndex = ref(0)
+	const tabBars = ref([{
+			name: '下载列表'
 		},
-		methods: {
-			
+		{
+			name: '上传列表'
 		}
+	])
+	const changeTab = (index) => {
+		tabIndex.value = index
+	}
+	
+	const list = ref([
+		{
+			type: 'image',
+			name: '风景.jpg',
+			create_time: '2023-07-01 21:26',
+			url: "https://i2.100024.xyz/2023/07/03/12zwnum.webp",
+			checked: false,
+			download: 100
+		},
+		{
+			type: 'video',
+			name: 'uniapp实战教程.mp4',
+			data: "https://flobby.oss-cn-shenzhen.aliyuncs.com/post/865691909-1-208.mp4",
+			create_time: '2023-07-01 19:26',
+			checked: false,
+			download: 66
+		},
+		{
+			type: 'image',
+			name: '风景.jpg',
+			create_time: '2023-07-01 21:26',
+			url: "https://i2.100024.xyz/2023/07/03/12zw8jj.webp",
+			checked: false,
+			download: 88
+		},
+		{
+			type: 'text',
+			name: '记事本.txt',
+			create_time: '2023-07-02 20:26',
+			checked: false,
+			download: 33
+		},
+		{
+			type: 'image',
+			name: '风景.jpg',
+			create_time: '2023-07-01 21:26',
+			url: "https://i2.100024.xyz/2023/07/03/12zwf5j.webp",
+			checked: false,
+			download: 50
+		},
+		{
+			type: 'none',
+			name: '压缩包.rar',
+			create_time: '2023-07-02 22:26',
+			checked: false,
+			download: 7
+		}
+	])
+	
+	// 下载状态属性
+	// 下载中
+	const downloading = computed(() => {
+		return list.value.filter(item => {
+			return item.download < 100;
+		})
+	})
+	// 下载完成
+	const downloaded = computed(() => {
+		return list.value.filter(item => {
+			return item.download === 100;
+		})
+	})
+	const swiperChange = (e) => {
+		tabIndex.value = e.detail.current
 	}
 </script>
 
-<style>
-
-</style>
+<template>
+	<view style="height: 100vh;" class="flex flex-column">
+		<!-- tab栏 -->
+		<view class="flex border-bottom border-light-secondary" style="height: 100rpx;">
+			<view class="flex-1 flex flex-column align-center justify-center" v-for="(item, index) in tabBars"
+				:key="index" :class="index === tabIndex ? 'text-main' : ''" @click="changeTab(index)">
+				<text> {{ item.name }} </text>
+				<text style="height: 8rpx;width: 140rpx;" class="rounded" :class="tabIndex === index ? 'bg-main' : 'bg-white'"></text>
+			</view>
+		</view>
+		
+		<!-- 列表内容 -->
+		<swiper :duration="250" class="flex flex-1" :current="tabIndex" @change="swiperChange">
+			<!-- 下载列表 -->
+			<swiper-item class="flex flex-1">
+				<scroll-view scroll-y="true" class="flex-1">
+					<view class="bg-light flex align-center font-sm px-2 text-muted">
+						文件下载至：storage/xxx/xxx
+					</view>
+					<view class="p-2 border-bottom border-light-secondary font text-muted">
+						下载中({{ downloading.length }})
+					</view>
+					<f-list v-for="(item, index) in downloading" :key="'i' + index" :item="item" :index="index">
+						<view class="flex align-center text-main" style="height: 70rpx;">
+							<text class="iconfont icon-zanting"></text>
+							<text class="ml-1">{{ item.download }}</text>
+						</view>
+						<progress slot="bottom" :percent="item.download" active-color="#009cff" :stroke-width="4" />
+					</f-list>
+					
+					<view class="p-2 border-bottom border-light-secondary font text-muted">
+						下载完成({{ downloaded.length }})
+					</view>
+					<f-list v-for="(item, index) in downloaded" :key="'d' + index" :item="item" :index="index" />
+				</scroll-view>
+			</swiper-item>
+			<!-- 上传列表 -->
+			<swiper-item class="flex flex-1">
+				<scroll-view scroll-y="true" class="flex-1">
+					<view class="font-md">
+						上传列表
+					</view>
+				</scroll-view>
+			</swiper-item>
+		</swiper>
+	</view>
+</template>
